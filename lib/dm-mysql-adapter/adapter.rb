028bb14768e940732ade1f934361a3a9f8090152
Daryn Warriner <daryn.warriner@firespring.com>
2024-04-30 02:15:45 -0500

    [story/TP-95093]: WIP: Rspec syntax updates

--- lib/dm-mysql-adapter/adapter.rb
+++ lib/dm-mysql-adapter/adapter.rb
@@ -3,9 +3,7 @@ require 'dm-do-adapter'
 
 module DataMapper
   module Adapters
-
     class MysqlAdapter < DataObjectsAdapter
-
       module SQL # :nodoc:
 
         IDENTIFIER_MAX_LENGTH = 64
@@ -19,7 +17,7 @@ module DataMapper
 
         # @api private
         def supports_subquery?(_query, _source_key, _target_key, _qualify)
-          # TODO: renable once query does not include target_model for deletes and updates
+          # TODO: re-enable once query does not include target_model for deletes and updates
           # query.limit.nil?
 
           false
@@ -34,14 +32,11 @@ module DataMapper
         def quote_name(name)
           "`#{name[0, self.class::IDENTIFIER_MAX_LENGTH].gsub('`', '``')}`"
         end
-
       end
 
       include SQL
-
     end
 
     const_added(:MysqlAdapter)
-
   end
 end
--- lib/dm-mysql-adapter/spec/setup.rb
+++ lib/dm-mysql-adapter/spec/setup.rb
@@ -4,12 +4,10 @@ require 'dm-core/spec/setup'
 module DataMapper
   module Spec
     module Adapters
-
       class MysqlAdapter < Adapter
       end
 
       use MysqlAdapter
-
     end
   end
 end
